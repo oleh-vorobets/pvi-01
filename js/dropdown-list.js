7cdc3f3876c1b89c030be4b29b3c7c8f2a33bb8c
oleh-vorobets <oleh.vorobets.pz.2022@lpnu.ua>
2024-02-17 18:51:05 +0200

    Added bell animation

--- js/dropdown-list.js
+++ js/dropdown-list.js
@@ -4,12 +4,17 @@ const userDropdown = document.querySelector('.user-dropdown');
 const bellMenu = document.querySelector('.bell-notification');
 const bellDropdown = document.querySelector('.notification-dropdown');
 
+const bellDot = document.querySelector('.bell-dot');
+const bell = document.querySelector('.bell');
+
 userMenu.addEventListener('click', function (e) {
     userDropdown.classList.toggle('closed');
     bellDropdown.classList.add('closed');
 });
 
 bellMenu.addEventListener('click', function (e) {
+    bellDot.classList.add('hidden');
+    bell.classList.add('paused-animation');
     bellDropdown.classList.toggle('closed');
     userDropdown.classList.add('closed');
 });
