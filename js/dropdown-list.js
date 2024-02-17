07fe7f014a43f532fb66f60f3be48d701a9b589b
oleh-vorobets <oleh.vorobets.pz.2022@lpnu.ua>
2024-02-17 15:55:25 +0200

    First version of messanger's navbar and menu item

---
+++ js/dropdown-list.js
@@ -0,0 +1,34 @@
+const userMenu = document.querySelector('.dropbtn');
+const userDropdown = document.querySelector('.user-dropdown');
+
+const bellMenu = document.querySelector('.bell');
+const bellDropdown = document.querySelector('.notification-dropdown');
+
+userMenu.addEventListener('click', function (e) {
+    if (
+        window.getComputedStyle(userDropdown).display === 'flex' &&
+        !userDropdown.contains(e.target)
+    ) {
+        userDropdown.style.display = 'none';
+    } else {
+        userDropdown.style.display = 'flex';
+    }
+});
+
+bellMenu.addEventListener('click', function (e) {
+    if (
+        window.getComputedStyle(bellDropdown).display === 'flex' &&
+        !bellDropdown.contains(e.target)
+    ) {
+        bellDropdown.style.display = 'none';
+    } else {
+        bellDropdown.style.display = 'flex';
+    }
+});
+
+document.addEventListener('click', function (e) {
+    if (!userMenu.contains(e.target) && !bellMenu.contains(e.target)) {
+        userDropdown.style.display = 'none';
+        bellDropdown.style.display = 'none';
+    }
+});
